6dceb87dd5a09bdb442643ea3f9338c416f2a877
benny <benpakmnz@gmail.com>
2019-06-11 01:08:29 +0300

    Updates

---
+++ precache-manifest.f0abdc4c62ed08c6b2c97ec48f3f9396.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6f16c65a910507e64a9ca54cfdfe5b7",
+    "url": "/music-store/index.html"
+  },
+  {
+    "revision": "b401dadfd325105edf4b",
+    "url": "/music-store/static/css/main.163b6494.chunk.css"
+  },
+  {
+    "revision": "3eff78bf8088d6013e69",
+    "url": "/music-store/static/js/2.5aff5406.chunk.js"
+  },
+  {
+    "revision": "b401dadfd325105edf4b",
+    "url": "/music-store/static/js/main.4408ab9e.chunk.js"
+  },
+  {
+    "revision": "e25b00c1ab0178940e6a",
+    "url": "/music-store/static/js/runtime~main.6d10df8e.js"
+  }
+]);
