afb19e8f3362630080f9f7fdfa5fcd08f425d83c
benny <benpakmnz@gmail.com>
2019-06-17 04:32:05 +0300

    Updates

---
+++ precache-manifest.3b033d27c44c0c0dd5c875ce29f566c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45f43b6e37214272e448376f85d8c98a",
+    "url": "/music-store/index.html"
+  },
+  {
+    "revision": "d84cfba8948c5a1dfa46",
+    "url": "/music-store/static/css/main.a80b0df4.chunk.css"
+  },
+  {
+    "revision": "1e81cf7d7ede086562e4",
+    "url": "/music-store/static/js/2.58ed6a8c.chunk.js"
+  },
+  {
+    "revision": "d84cfba8948c5a1dfa46",
+    "url": "/music-store/static/js/main.797192ab.chunk.js"
+  },
+  {
+    "revision": "e25b00c1ab0178940e6a",
+    "url": "/music-store/static/js/runtime~main.6d10df8e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/music-store/precache-manifest.c4962a51a5f26f6db76542af7383a380.js"
+  "/music-store/precache-manifest.3b033d27c44c0c0dd5c875ce29f566c7.js"
 );
 
 self.addEventListener('message', (event) => {
